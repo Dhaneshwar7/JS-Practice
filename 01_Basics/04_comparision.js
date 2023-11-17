be447430d505f750713c6b7597bd2828cd9891cc
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2023-11-17 20:50:13 +0000

    comparision confusing part

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,22 @@
+/*  Comparisons  */
+// console.log(2> 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 === 1);
+// console.log(2 != 1);
+
+// console.log('2' > 1);
+// console.log('02' > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+console.log('2' === 2); // true
+console.log('2' == 2); // false
+// === strict check with Datatypes

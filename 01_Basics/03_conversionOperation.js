b63bcfdcc4095abcbf7ea1a438f5b21e0e8c5e9d
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2023-11-17 19:51:59 +0000

    JavaScript Conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,85 @@
+// /* For Checking this do in Window (Ctrl+All then Ctrl+/) */
+// /* For Checking this do in Window (cmd+All then cmd+/) */
+// /* Simple one  */
+// let score = 33;
+// console.log(typeof score); //this will print Number
+// console.log(typeof(score)); //this will print Number
+
+// /* "34abd" */
+// /* String value Converstion */
+// let run = "50"; // this is String value but we can change this into Number
+// let convertRun = Number(run); // this is conversion of any string into Number
+// console.log(convertRun); // Print => 50
+// console.log(typeof convertRun); // Print => number
+
+// /* NAN => NOT A NUMBER */
+// /* Complicate when String value Converstion if something like "5600dfjkj" or any */
+// let userScore = "80bbc"
+// let valueinNumber = Number(userScore) // this convert userScore in Number 
+// console.log(typeof valueinNumber); //this is still showing that is converted to a number 
+// console.log(valueinNumber); // NaN
+
+
+// /* NULL null */
+// /* What will happen with NULL */
+// let check= null
+// let valueCheck = Number(check) // print number
+// console.log(typeof valueCheck); //  0
+// console.log(valueCheck);
+
+
+// /* Undefined */
+// /* Let check what happen with Undefined */
+// let unde = undefined
+// let valueUnde = Number(unde);
+// console.log(typeof valueUnde);  // print number
+// console.log(valueUnde); // NaN
+
+
+// /* Boolean True/False */
+// /* Let check what happen with Boolean */
+// let bool = false
+// let valueBool = Number(bool);
+// console.log(typeof valueBool);  // print number
+// console.log(valueBool); // 1 depends of true , 0 false 
+
+
+
+// /* Booloean Conversion */
+// let islogedIN = "ok";
+// let booleandIslogedin = Boolean(islogedIN);
+// console.log(booleandIslogedin);
+
+// /* String Conversion */
+// let stringNum = 33 ;
+// let valueStringNum = String(stringNum);
+// console.log(valueStringNum);
+// console.log(typeof valueStringNum);
+
+// /* Conclusion */
+
+// /* when converted in to Number */
+// //  "33"    => 33
+// // "33abd"  => NaN
+// // true     => 1  , false => 0
+// // undefine => NaN
+// //  null    => 0
+
+// /* when converted in to Boolean */
+// // 1    => true
+// // 0    => false
+// // ""   => false
+// // "ok" => true
+
+// /* when converted in to String */
+// // 1    => String 1
+// // 0    => String 0 
+// // ''   => 
+// // "ok" => String ok
+// // null => String null
+// // NaN  => String NaN
+// // Undefined =>  undefined is not defined
+
+
+
+

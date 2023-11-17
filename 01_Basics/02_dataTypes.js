ccdb7be3403130189d21e1fd753019c6d1b2f54a
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2023-11-17 19:09:23 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,41 @@
+"use strict" //treat all JS Code as newer Version
+
+// alert( 3+3 ) // we are using nodejs , not browser
+
+// console.log( 3 + 
+    // 3)   -----> make Sure Code Readiablity should be High
+console.log("delta");
+
+/* 
+For JS Documentation 
+1. tc39 (ECMA STANDARDS)
+2. MDN - MOZILLA
+*/
+
+/* Now DATATYPES In JavaScript */
+
+let name = "deltaMike" 
+let age = 24
+let isLoggedIn = true
+let somethingValue;
+
+
+/* Primitive Datatypes */
+// --------
+// String name or any text in String --->   "abc"  / 'abc'
+// Number => 2 to power 53 valid
+// Boolean => true/false (Simple ha ya nahi)
+// Null =>  Standalone Value
+// Undefined => not defined any value
+
+
+
+/* Non-Primitive Datatypes */
+// -----
+// Object
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //Object
+
+

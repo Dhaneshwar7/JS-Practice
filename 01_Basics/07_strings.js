22acaa74bd72e25817d418d78147b1e5e8769ee0
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2023-11-28 07:12:25 +0000

    Strings topic updated

--- 01_Basics/07_strings.js
+++ 01_Basics/07_strings.js
@@ -23,7 +23,7 @@ console.log(myName.substring());
 
 /* Method of String 
  */
-let myTwoName = new String('DivyaprakashPal');
+let myTwoName = new String('DivyaprakashPal-Bairagarh-Coder');
 
 let inpmyTwoName = myTwoName.substring(0,4)
 let newSlice = myTwoName.slice(3 ,-7)
@@ -34,9 +34,25 @@ let newSpace = "      hello.  "
 console.log(newSpace);
 console.log(newSpace.trim());
 
+const url = "deltamike@-20developer.com"
+
+let newReplaceValue = url.replace("-20" , "codeer.")
+let newIncludeValue = url.includes("ike")
+let newSliptValue = myTwoName.split("-")
+console.log(newReplaceValue);
+console.log(newIncludeValue);
+console.log(newSliptValue);
+
+
+
 /*
 substring()
-s
-charAt
-
+indexOf()
+charAt()
+slice()
+split()
+trim()
+replace()
+include()
+many more methods......
   */

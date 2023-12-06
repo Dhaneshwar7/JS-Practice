7e37e3dc7ece1865e73e4a1222a296bf7acc04c9
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2023-12-06 09:03:45 +0000

    Array Topic

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,48 @@
+let Arr = [1,2,3,4,5 , "delta", true , null , 45]
+// console.log(Arr);
+// console.log(Arr[4]);
+// console.log(typeof Arr);
+
+let newArr = new Array(1,23,95 ,69 ,2); 
+newArr.push(98)
+console.log(newArr);
+// newArr.pop()
+// console.log(newArr);
+
+newArr.unshift("Bharat") // add the element at the start an Array
+// console.log(newArr);
+newArr.shift()
+// console.log(newArr);
+
+// console.log(newArr.includes(2));
+// console.log(newArr.indexOf(69));
+
+const newJoinArr = newArr.join()
+console.log(newJoinArr);
+console.log(typeof newJoinArr);
+
+
+/* Slice And Splice  */
+
+// Slice cuts the Array index value by giving 2 parameter and nothing changes in main Array
+
+//Example for Slice
+let okArr = [1,2,3,4,5,6,7];
+// console.log('A', okArr);
+
+// console.log(okArr.slice(1,3))
+// console.log('B', okArr);
+
+// //Example for Splice
+// console.log(okArr.splice(1, 4));
+// console.log('C' , okArr)
+
+
+
+
+
+
+
+// Splice cuts the Array Index value 
+
+

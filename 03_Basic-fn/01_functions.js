bac60c6369987b4615528c1510ad288722dc94c3
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2023-12-19 14:51:45 +0000

    function Basics

---
+++ 03_Basic-fn/01_functions.js
@@ -0,0 +1,80 @@
+/* ----- Functions in Javascripts.  ----- */
+
+// console.log("D");
+// console.log("E");
+// console.log("L");
+// console.log("T");
+// console.log("A");
+
+// result : DELTA in 5lines
+
+// ** what if we need 6 times so there comes Funtions a specific code store in the container whenever we call that will be Execute
+
+function printMyname() {
+  console.log("D");
+  console.log("E");
+  console.log("L");
+  console.log("T");
+  console.log("A");
+}
+// printMyname // if we call like this - means Reference (nothing print)
+// printMyname() // if we call like this - means Fn Executes (print whatever inside the fn)
+// printMyname()
+// printMyname()
+// printMyname() //now we can print as many as we want
+
+// Example: 1
+
+function addNumber(num1, num2) {
+  //(a,b) ----> Parameters
+  console.log(num1 + num2);
+}
+// addNumber(3,4) //----> passing Arguments
+// addNumber(3,"o")
+// addNumber(3, null)
+//in this type of funtions Datatype manipulation probleam create but it can be handle
+
+// Example: 2 We can Store the value in variable
+
+/*  ----- Type 1 ------ */
+// function addTwoNumber(num1 ,num2){
+//     // console.log(num1 + num2);
+// }
+// const result  = addTwoNumber(3,4)
+// console.log("Result: "result); // gives us undefined
+
+/*  ----- Type 2 ------ */
+function addTwoNumber(num1, num2) {
+  //(a,b) ----> Parameters
+  // let result = num1 +num2;
+  // return result ;
+
+  // console.log("ok prints"); //this won't print if written after return
+  return num1 + num2;
+  console.log("not print"); //this won't print if written after return
+}
+const result = addTwoNumber(3, 4);
+// console.log("Result: ", result); // gives us 7/ or calculation ans return value
+
+// Example: 3
+
+function loggedInUser(username) {
+  return `${username} is Logged in Successfully ,Welcome.`;
+}
+// loggedInUser("MikeCoder"); // not print because it exucated but not consoled
+// console.log(loggedInUser("MikeCoder")); // Will Prints in console
+// console.log(loggedInUser("")); // Will Prints other written string
+// console.log(loggedInUser()); // Will Prints Undefined + further string
+
+//Example: 4  Check if user is availber or not through if Else
+
+function loginUser(username = "noUser") { // default if no arguments pass
+  if (username === undefined) {
+console.log(`Please enter a username `)
+return
+  }
+  return `${username}, is logged in `
+}
+
+// console.log(loginUser("ok"))
+console.log(loginUser()) // please enter a username  + undefined

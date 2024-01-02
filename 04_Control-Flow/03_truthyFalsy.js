cd8c4c9d731c65ccb36fc946c1359ac5efd6afd4
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2024-01-02 16:28:07 +0000

    flow final

--- 04_Control-Flow/03_truthyFalsy.js
+++ 04_Control-Flow/03_truthyFalsy.js
@@ -15,10 +15,26 @@ const obj = {};
 // Check with Array
 if (arr.length === 0) {
 	console.log('yaa array is Blank');
-}   
+}
 
 if (Object.keys(obj).length === 0) {
 	console.log('yaa Objaect is Blank');
 } else {
 	console.log('yaa Objaect is Not-Blank');
 }
+
+/* Nullish Coelescing Operation ?? : null undefined  */
+
+let value;
+// value = 5 ?? 10;    //-> 5
+// value = null ?? 10;  //->  10
+// value = undefined ?? 15;   //-> 15
+value = null?? 10 ??20;    //-> 10first value after null
+console.log(value);
+
+
+/* -- Ternary Operator ? short hand for if else */
+
+// condition ? true : false ;
+let marks =25;
+marks <=33 ? console.log("You are FAIl") : console.log('YOu are Passed the Exam');

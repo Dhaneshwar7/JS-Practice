9e9bef01912329e7c31e0ab86dd287abd343de8f
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2024-01-02 18:18:23 +0000

    while do while

---
+++ 05_iterations/02_whiledo.js
@@ -0,0 +1,19 @@
+/* While Loop */
+let score = 5;
+// while (score <= 21) {
+// 	console.log(`Score is ${score}`);
+//     score = score + 2;
+// }
+
+/* While Loop in Arry */
+let myArr = ["Bhagat Sing" , "Sahdev", "Chandra Shekhar", "Subhash Chandra Bose"]
+let flag = 0
+while (flag< myArr.length ){
+    console.log(`Our Freedom Fighters ${myArr[flag]}`);
+    flag++
+}
+let marks = 5;
+do {
+    console.log(`OK print ${marks}`); /// ek bar to chalna hi hai
+    marks++
+} while (marks < 3);

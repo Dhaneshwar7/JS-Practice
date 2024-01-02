0ebdc797d55dcd7c0c5512ce46b4cf8860ef32e9
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2024-01-02 15:15:55 +0000

    switch conditional

---
+++ 04_Control-Flow/02_switch.js
@@ -0,0 +1,20 @@
+const month = '02';
+switch (month) {
+	case '01':
+		console.log('Janaury');
+		break;
+	case '02':
+		console.log('February');
+		break;
+	case '03':
+		console.log('March');
+		break;
+	case '04':
+		console.log('April');
+		break;
+	case '05':
+		console.log('May');
+		break;
+	default:
+		break;
+}

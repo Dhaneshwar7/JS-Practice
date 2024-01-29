773d4f37639af022935d473cbbf8ce5a7d6b5cda
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2024-01-29 18:20:40 +0000

    object v2

--- 11_Classes&OOPs/03_object.js
+++ 11_Classes&OOPs/03_object.js
@@ -6,3 +6,18 @@ multipleBy5.power = 2;
 console.log(multipleBy5(6)); //30
 console.log(multipleBy5.power); //2
 console.log(multipleBy5.prototype); // {}
+
+
+function createUser(username ,score ){
+    this.username = username
+    this.score = score
+}
+createUser.prototype.increment =()=>{
+    this.score++
+}
+createUser.prototype.printMe=()=>{
+    console.log(`Score is ${this.score}`);
+}
+
+const pahla = createUser("Bheem" , 98)
+const dusra = createUser("kaliya" , 90)

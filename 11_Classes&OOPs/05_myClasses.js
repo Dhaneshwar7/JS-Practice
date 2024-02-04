09b2856af05233b589eb7141f6af00f2a2b50202
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2024-02-04 17:28:28 +0000

    class constructor and function constructor

--- 11_Classes&OOPs/05_myClasses.js
+++ 11_Classes&OOPs/05_myClasses.js
@@ -1,15 +1,41 @@
-
+// ES 6 
+/* Class Constructors */
 class User{
     constructor(username ,email,password){
         this.username = username
         this.email= email
         this.password = password
     }
-
     encryptPassword (){
         return `${this.password}abc`
     }
+    changeUsername (){
+        return `${this.username.toUpperCase()}`
+    }
 }
 
-const pahlaUser = new User("Delat" , "mike@gmial.m", "001")
-console.log(pahlaUser);
+const pahlaUser = new User("Delta" , "mike@gmial.m", "001")
+console.log(pahlaUser.encryptPassword());
+console.log(pahlaUser.changeUsername());
+
+
+/* -- Behind the Scene in function --- */
+
+function Client(clientName, clientEmail, clientPassword){
+    this.clientName = clientName
+    this.clientEmail= clientEmail
+    this.clientPassword = clientPassword
+}
+
+Client.prototype.clientPasswordchange = function(){
+    return `${this.clientPassword}abc`
+}
+Client.prototype.clientNamechange = function(){
+    return `${this.clientName}abc`
+}
+const firstClient = new Client("Amar" , "Amar@gmail.com", "23434")
+console.log(firstClient.clientPasswordchange())
+console.log(firstClient.clientNamechange())
+
+
+
